a596d2ac814c20192b7f73f7e10299e389825223
fshstk <fshstk@fshstk.com>
2020-10-09 00:59:04 +0200

    Auto-formatting

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,29 +3,30 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (600, 400);
+  // Make sure you set the size of the component after
+  // you add any child components.
+  setSize(600, 400);
 }
 
-MainComponent::~MainComponent()
-{
-}
+MainComponent::~MainComponent() {}
 
 //==============================================================================
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+  // (Our component is opaque, so we must completely fill the background with a
+  // solid colour)
+  g.fillAll(
+    getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+  g.setFont(juce::Font(16.0f));
+  g.setColour(juce::Colours::white);
+  g.drawText(
+    "Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+  // This is called when the MainComponent is resized.
+  // If you add any child components, this is where you should
+  // update their positions.
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,21 +7,20 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent : public juce::Component
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
+  //============================================================================
+  MainComponent();
+  ~MainComponent() override;
 
-    //==============================================================================
-    void paint (juce::Graphics& g) override;
-    void resized() override;
+  //============================================================================
+  void paint(juce::Graphics& g) override;
+  void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+  //============================================================================
+  // Your private member variables go here...
 
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
