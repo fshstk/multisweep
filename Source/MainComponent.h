311bbdc4cbba86b2f09946cd65f3440d35c04668
fshstk <fshstk@fshstk.com>
2020-10-09 01:08:09 +0200

    Add audio capability (import modules and add functions).

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,12 +7,18 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent : public juce::Component
+class MainComponent : public juce::AudioAppComponent
 {
 public:
   //============================================================================
   MainComponent();
   ~MainComponent() override;
+
+  //============================================================================
+  void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+  void getNextAudioBlock(
+    const juce::AudioSourceChannelInfo& bufferToFill) override;
+  void releaseResources() override;
 
   //============================================================================
   void paint(juce::Graphics& g) override;
