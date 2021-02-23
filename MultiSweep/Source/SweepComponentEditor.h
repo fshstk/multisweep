23a730339eb22eedb8759e6b96f5df9e211f5907
fshstk <fshstk@fshstk.com>
2021-02-23 13:01:48 +0100

    Replace audio thumbnail with proper freq response graph

--- MultiSweep/Source/SweepComponentEditor.h
+++ MultiSweep/Source/SweepComponentEditor.h
@@ -24,6 +24,7 @@
 #include "SweepComponentProcessor.h"
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
+#include <lookAndFeel/IEM_LaF.h>
 
 /*
 
@@ -37,6 +38,121 @@ separate class: audio thumbnail, takes in buffer/vector by reference,
                 has a callback to redraw
 
 */
+
+class FreqResponseDisplay
+  : public juce::Component
+  , private juce::ChangeListener
+{
+public:
+  FreqResponseDisplay(SweepComponentProcessor& processor)
+    : sweep(processor)
+  {
+    sweep.addThumbnailListener(this);
+  }
+  ~FreqResponseDisplay() override { sweep.removeThumbnailListener(this); }
+
+  void paint(juce::Graphics& g) override
+  {
+    const auto curve = sweep.getFrequencyResponse();
+
+    const auto margin = 35;
+    const auto area = getLocalBounds();
+
+    const auto graph = area.reduced(margin);
+    const auto xAxis = dft_log_bins(graph.getWidth(), 20.0, 20e3);
+
+    auto yAxis = std::vector<float>(graph.getHeight());
+    std::iota(yAxis.begin(), yAxis.end(), 0);
+    std::transform(
+      yAxis.cbegin(), yAxis.cend(), yAxis.begin(), [&yAxis](auto x) {
+        const auto dB_low = -24;
+        const auto dB_high = 24;
+        return x * (dB_high - dB_low) / yAxis.size() + dB_low;
+      });
+    const auto yValues =
+      std::vector<float>{ -24, -18, -12, -6, 0, 6, 12, 18, 24 };
+    const auto findPixelForDb = [&yAxis](auto value) {
+      return std::distance(
+        yAxis.cbegin(), std::lower_bound(yAxis.cbegin(), yAxis.cend(), value));
+    };
+    auto yPixels = std::vector<int>(yValues.size());
+    std::transform(
+      yValues.cbegin(), yValues.cend(), yPixels.begin(), findPixelForDb);
+
+    const auto values =
+      std::vector<float>{ 20,  30,  40,  50,  60,  70,  80,   90,  100, 200,
+                          300, 400, 500, 600, 700, 800, 900,  1e3, 2e3, 3e3,
+                          4e3, 5e3, 6e3, 7e3, 8e3, 9e3, 10e3, 20e3 };
+    const auto labeledValues = std::vector<float>{ 20, 200, 2e3, 20e3 };
+
+    const auto findPixelForFrequency = [&xAxis](auto value) {
+      return std::distance(
+        xAxis.cbegin(), std::lower_bound(xAxis.cbegin(), xAxis.cend(), value));
+    };
+
+    auto xPixels = std::vector<int>(values.size());
+    std::transform(
+      values.cbegin(), values.cend(), xPixels.begin(), findPixelForFrequency);
+
+    // g.fillAll(juce::Colours::blanchedalmond);
+
+    g.setColour(juce::Colours::darkslategrey);
+    g.fillRect(graph);
+    g.setColour(lookAndFeel.ClSeperator);
+    g.drawRect(graph);
+
+    for (auto& pixel : xPixels) {
+      const auto x = pixel + graph.getX();
+      const auto y1 = graph.getY();
+      const auto y2 = graph.getBottom();
+
+      g.drawLine(x, y1, x, y2);
+    }
+
+    for (auto& pixel : yPixels) {
+      const auto y = pixel + graph.getY();
+      const auto x1 = graph.getX();
+      const auto x2 = graph.getRight();
+
+      g.drawLine(x1, y, x2, y);
+    }
+
+    for (auto& value : labeledValues) {
+      const auto x = graph.getX() + findPixelForFrequency(value);
+      const auto y = graph.getBottom() + margin / 2;
+
+      const auto label = value < 1000 ? juce::String(value) + " Hz"
+                                      : juce::String(value / 1000) + " kHz";
+
+      g.setFont(lookAndFeel.robotoMedium);
+      g.setColour(lookAndFeel.ClText);
+      g.drawSingleLineText(label, int(x), int(y), juce::Justification::centred);
+    }
+
+    for (auto& value : yValues) {
+      const auto x = graph.getX() - 5;
+      // const auto y = graph.getBottom() - findPixelForDb(value);
+      const auto y = graph.getBottom() - findPixelForDb(value) + 3;
+
+      const auto label = juce::String(value) + " dB";
+
+      g.setFont(lookAndFeel.robotoMedium);
+      g.setColour(lookAndFeel.ClText);
+      g.drawSingleLineText(label, int(x), int(y), juce::Justification::right);
+    }
+  }
+
+private:
+  void changeListenerCallback(juce::ChangeBroadcaster*) override { repaint(); }
+
+private:
+  LaF lookAndFeel;
+  SweepComponentProcessor& sweep;
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FreqResponseDisplay)
+};
+
+//==============================================================================
 
 // Borrowed from the JUCE examples:
 class RecordingThumbnail
@@ -130,7 +246,7 @@ public:
   SweepComponentEditor(SweepComponentProcessor& sweepProcessor)
     : AudioProcessorEditor(&sweepProcessor)
     , sweep(sweepProcessor)
-    , sweepResponseThumbnail(sweepProcessor)
+    , freqResponse(sweepProcessor)
   {
     addAndMakeVisible(playButton);
     playButton.setButtonText("Start Sweep");
@@ -168,7 +284,7 @@ public:
     durationSliderLabel.setJustificationType(juce::Justification::centred);
     durationSliderLabel.attachToComponent(&durationSlider, false);
 
-    addAndMakeVisible(sweepResponseThumbnail);
+    addAndMakeVisible(freqResponse);
   }
   void resized() override
   {
@@ -180,7 +296,7 @@ public:
     auto secondButtonRow = bottomRow;
 
     durationSlider.setBounds(sliderArea);
-    sweepResponseThumbnail.setBounds(area.reduced(25));
+    freqResponse.setBounds(area);
 
     playButton.setBounds(
       firstButtonRow.removeFromLeft(firstButtonRow.getWidth() / 2));
@@ -201,7 +317,7 @@ private:
   juce::Slider durationSlider;
   juce::Label durationSliderLabel;
 
-  RecordingThumbnail sweepResponseThumbnail;
+  FreqResponseDisplay freqResponse;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SweepComponentEditor)
 };
