1ada7f4d303ebddd0b391665d6616837aa10278e
fshstk <fshstk@fshstk.com>
2021-03-05 00:58:29 +0100

    Add test for freq response calc (failing)

--- MultiSweep/Test/FilterParameterTest.cpp
+++ MultiSweep/Test/FilterParameterTest.cpp
@@ -5,12 +5,33 @@
 
 TEST_CASE("Check frequency response calculation")
 {
-  const auto frequencies = std::vector<double>{};
-  const auto filters = std::vector<FilterParameter>{};
+  auto frequencies = std::vector<double>(110252);
+  std::iota(frequencies.begin(), frequencies.end(), 0);
+  std::transform(frequencies.begin(),
+                 frequencies.end(),
+                 frequencies.begin(),
+                 [](auto x) { return x * 22050 / 110251; });
 
-  const auto reference_response = std::vector<double>{};
-  const auto calculated_response =
+  // Taken from sweeptest.csv:
+  REQUIRE(frequencies[0] == Approx(0.0));
+  REQUIRE(frequencies[1] == Approx(0.199998));
+  REQUIRE(frequencies[79390] == Approx(15877.9));
+  REQUIRE(frequencies[98480] == Approx(19695.8));
+  REQUIRE(frequencies[110250] == Approx(22049.8));
+
+  const auto filters = std::vector<FilterParameter>{
+    FilterParameter{ .frequency = 150, .gain = +5.0, .q_factor = 1.0 },
+    FilterParameter{ .frequency = 900, .gain = -7.0, .q_factor = 1.0 },
+    FilterParameter{ .frequency = 3e3, .gain = +6.0, .q_factor = 1.0 },
+  };
+
+  const auto magnitudes =
     FilterApproximation::calculate_frequency_response(filters, frequencies);
 
-  REQUIRE(calculated_response == reference_response);
+  // Taken from sweeptest.csv:
+  REQUIRE(frequenmagnitudescies[0] == Approx(0.0386788));
+  REQUIRE(magnitudes[1] == Approx(0.0389565));
+  REQUIRE(magnitudes[79390] == Approx(1.01112));
+  REQUIRE(magnitudes[98480] == Approx(1.00539));
+  REQUIRE(magnitudes[110250] == Approx(0.4324));
 }
