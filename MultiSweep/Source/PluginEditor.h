59ef73da9a75c2e1e4610e9cb42b106b49ab0b23
fshstk <fshstk@fshstk.com>
2022-01-30 20:19:09 +0100

    Remove IEM plug-in suite comment

--- MultiSweep/Source/PluginEditor.h
+++ MultiSweep/Source/PluginEditor.h
@@ -1,25 +1,3 @@
-/*
- ==============================================================================
- This file is part of the IEM plug-in suite.
- Authors: Fabian Hummel, David Neussl
- Copyright (c) 2020 - Institute of Electronic Music and Acoustics (IEM)
- https://iem.at
-
- The IEM plug-in suite is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- The IEM plug-in suite is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this software.  If not, see <https://www.gnu.org/licenses/>.
- ==============================================================================
- */
-
 #pragma once
 #include "PluginProcessor.h"
 #include "SingleChannelIOWidget.h"
