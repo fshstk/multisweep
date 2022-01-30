11ef833f98548e898f1749d1a6e77fdf43f6fbc1
fshstk <fshstk@fshstk.com>
2022-01-30 18:50:36 +0100

    Dynamically load fftw3 from the web

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -38,5 +38,10 @@ include_directories(${NLOPT_INCLUDE_DIRS})
 # fftw3
 ################################################################################
 
-include(FindPkgConfig)
-pkg_check_modules(fftw3 REQUIRED IMPORTED_TARGET fftw3)
+FetchContent_Declare(
+  fftw3
+  URL       http://fftw.org/fftw-3.3.10.tar.gz
+  URL_HASH  MD5=8ccbf6a5ea78a16dbc3e1306e234cc5c)
+FetchContent_MakeAvailable(fftw3)
+
+include_directories(${fftw3_SOURCE_DIR}/api)
