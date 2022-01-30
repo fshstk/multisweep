5c313c73520cbd39b390abb770a733efc012377b
fshstk <fshstk@fshstk.com>
2022-01-30 18:14:35 +0100

    Include Juce/Catch2 via Dependencies.cmake

---
+++ Dependencies.cmake
@@ -0,0 +1,28 @@
+################################################################################
+# FetchContent
+################################################################################
+
+include(FetchContent)
+
+################################################################################
+# JUCE
+################################################################################
+
+FetchContent_Declare(
+  JUCE
+  GIT_REPOSITORY https://github.com/juce-framework/JUCE
+  GIT_TAG        6.1.4)
+FetchContent_MakeAvailable(JUCE)
+
+################################################################################
+# Catch2
+################################################################################
+
+FetchContent_Declare(
+  Catch2
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG        v2.13.7)
+FetchContent_MakeAvailable(Catch2)
+
+list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
+include(Catch)
